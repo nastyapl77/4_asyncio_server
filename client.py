26751dba3bb2e73c68bb9062299e1270818955d4
nastyapl77 <55870686+nastyapl77@users.noreply.github.com>
2019-11-06 22:09:17 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,23 +1,23 @@
 import asyncio
 
 
-HOST = 'localhost'
-PORT = 9095
-
-
 async def tcp_echo_client(host, port):
     reader, writer = await asyncio.open_connection(host, port)
-    message = 'Hello, world'
+    message = input('Enter message for server: ')
 
     writer.write(message.encode())
     await writer.drain()
 
     data = await reader.read(100)
+    print(data, 'returned from server')
     writer.close()
-    # await writer.wait_closed()
+    await writer.wait_closed()
 
-# asyncio.run(tcp_echo_client(HOST, PORT))
 
-loop = asyncio.get_event_loop()
-task = loop.create_task(tcp_echo_client(HOST, PORT))
-loop.run_until_complete(task)
+HOST = 'localhost'
+PORT = 5555
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    task = loop.create_task(tcp_echo_client(HOST, PORT))
+    loop.run_until_complete(task)
